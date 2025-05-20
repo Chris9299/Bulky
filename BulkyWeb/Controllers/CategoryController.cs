22086ed41b5d1aca55b0b40cf6cd724e9fbf4c35
cmerlin <chrismerlin.p@iqvia.com>
2025-05-20 09:34:38 -0400

    add

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -16,5 +16,24 @@ namespace BulkyWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+        public IActionResult Create() {
+        
+        return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj) {
+            //if(obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name","DisplayOrder Cannot Match the category name");
+            //}
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
